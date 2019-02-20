2c3fec4b054cb92fb41a0738d3299713b1b973b7
Alexander Desyatnichenko <1_0@usa.com>
2019-02-20 14:10:30 +0200

    Start calc

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String startString = new String("Type math expression ('a + b', 'a-b', 'a * b', 'a / b') or 'exit':");
+        System.out.println(startString);
+    }
+}
