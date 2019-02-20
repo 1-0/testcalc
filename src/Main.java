45125755b2e3350d99e180a81c1823193ff7b2a4
Alexander Desyatnichenko <1_0@usa.com>
2019-02-20 15:03:23 +0200

    Add take operations

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String startString = new String("Type math expression ('a + b', 'a-b', 'a * b', 'a / b') or 'exit':");
+        String startString = new String("Type math expression ('a+b', 'a-b', 'a*b', 'a/b') or 'exit':");
         String exitString = new String("exit");
         String exitingString = new String("Exiting...");
+        String wrongString = new String("Wrong statement");
+        int firstNum, secondNum;
+        String operation;
         while (true) {
             System.out.println(startString);
             Scanner s = new Scanner(System.in);
@@ -13,6 +16,18 @@ public class Main {
             if ( exitString.equals(inputStr)) {
                 System.out.println(exitingString);
                 System.exit(0);
+            }
+            if(inputStr.contains("+")){
+                operation = "+";
+            } else if (inputStr.contains("-")) {
+                operation = "-";
+            } else if (inputStr.contains("*")) {
+                operation = "*";
+            } else if (inputStr.contains("/")) {
+                operation = "/";
+            } else {
+                System.out.println(wrongString);
+                continue;
             }
         }
     }
